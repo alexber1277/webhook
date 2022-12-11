39b99535e9785f1c2f8558d20062a5132fffc933
Alex <alexber127@gmail.com>
2022-12-11 21:09:29 +0300

    ...

--- main.go
+++ main.go
@@ -9,15 +9,16 @@ import (
 
 func main() {
 	var (
-		reader io.Reader
-		url    = os.Getenv("PLUGIN_URL")
+		reader  io.Reader
+		hostApi = os.Getenv("PLUGIN_HOST_API")
+		token   = os.Getenv("PLUGIN_TOKEN")
 	)
-
-	url = url
-
-	_, err := http.Post("http://178.154.204.138:9000/api/webhooks/19feb2cf-bb14-49dc-9f00-d8e670b74dea?tag=latest", "application/json", reader)
+	_, err := http.Post(hostApi+token+"?tag=latest", "application/json", reader)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 }
+
+// http://178.154.204.138:9000/api/webhooks/
+// 19feb2cf-bb14-49dc-9f00-d8e670b74dea
